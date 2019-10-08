0353233cff49df8ac87297e316356bf78b5e70c0
hunterhann2010 <josh.m.hunter@gmail.com>
2019-10-08 11:54:37 -0700

    working on GET ALL dinos route

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const fs = require('fs');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static('static'));
+
+
+app.get('/', function(req, res) {
+    res.send(`You hit the root route`);
+});
+
+app.use('/dinosaurs', require('./routes/dinosaurs'));
+
+
+
+
+app.listen(3000, function (){
+    console.log('you are listening to the sweet sounds of 3000')
+})
